edd5da3aad5333326911f3a83d2dfd2dd6865854
amitkumar7245 <amitkdna@gmail.com>
2025-07-12 18:01:50 +0530

    website update

--- source/conf.py
+++ source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Crypto Support'
+project = 'CryptoSupport'
 copyright = '2025, Author'
 author = 'Author'
 
